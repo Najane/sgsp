26ae057d9f7c600478b68ae917dc8519c8d2dc9b
Najane <najanelopez@gmaill.com>
2018-11-29 20:46:34 -0400

    Cor deixou de ser tabela

--- sgsp/api/models/Servidor.js
+++ sgsp/api/models/Servidor.js
@@ -41,7 +41,7 @@ module.exports = {
 
     dataPosse: { type: 'string', columnType: 'date' },
 
-    //codigoEnd: { type: 'Endereco' },
+    descricaoCor: { type: 'string' },
 
     //codigoGrau: { type: 'GrauInstrucao' },
 
