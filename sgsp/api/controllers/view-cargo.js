cbc12021b427931e4f0a5708c3c287132b035ff0
Najane <najanelopez@gmail.com>
2018-12-01 17:37:06 -0400

    Envia variavel da controller para view

--- sgsp/api/controllers/view-cargo.js
+++ sgsp/api/controllers/view-cargo.js
@@ -1,26 +1,24 @@
 module.exports = {
 
-
   friendlyName: 'View cargo',
 
-
   description: 'Display "Cargo" page.',
-
 
   exits: {
 
     success: {
       viewTemplatePath: 'pages/cargo'
     }
-
   },
 
 
   fn: async function (inputs, exits) {
 
     // Respond with view.
-    return exits.success(); 
-
+    var cargos = await Cargo.find();
+    return exits.success({
+      cargos: cargos
+    });
   }
 
 
